e2256b3a962e03b1acad2ba002c9da2da414d9a2
davimuto <davimuto@gmail.com>
2024-03-10 19:39:46 +0100

    add: bonus input select per inserire l'eta + tasto (submit) al posto di button impossibiltia di refresh pagina con prevent Default ; aggiunto tag form nell Html

--- js/app.js
+++ js/app.js
@@ -1,25 +1,23 @@
 
-
-
-
-    //richiama dall HTML gli elementi tramite id
-const buttonElement = document.getElementById('calcola')
+// richiama dall HTML gli elementi tramite id
+const buttonElement   = document.getElementById('calcola')
 const kmInputElement  = document.getElementById('km')
 const etaInputElement = document.getElementById('eta')
-const resultElement  = document.getElementById ('risultato')
+const resultElement   = document.getElementById ('risultato')
  
     // esegui tutto il "calcolo js" premendo l elemento button dell html (calcola)
-buttonElement.addEventListener('click', function (){
+buttonElement.addEventListener('click', function(event) {
+    event.preventDefault();
 
     //recupero con .value i dati inseriti dall utente
-const eta = parseInt( etaInputElement.value)
+const eta = etaInputElement.value
 const km = parseInt(kmInputElement.value)
 
     // costanti di prezzi base e sconti (calcolo base km per prezzo base)
 const prezzoBase = 0.21
 const prezzoPerKm = prezzoBase * km
-const sconto20 = prezzoPerKm - 0.2
-const sconto40 = prezzoPerKm - 0.4
+const sconto20 = prezzoPerKm - (prezzoPerKm * 0.2) 
+const sconto40 = prezzoPerKm - (prezzoPerKm * 0.4) 
 
 
     // stampa elementi in tabella sul dom
@@ -27,13 +25,13 @@ kmElement.innerHTML = (km)
 etaElement.innerHTML = (eta)
 prezzoElement.innerHTML =((prezzoPerKm).toFixed(2) + '€') 
 
-   // calcola il prezzo effettivo in base all eta 
-if (!isNaN(km) && !isNaN(eta) && eta > 0 &&  km > 0 && eta < 110) {  
-  if (eta < 18) {
+// calcola il prezzo effettivo in base all eta 
+if (!isNaN(km)  && km > 0 ) {  
+  if (eta === 'minorenne') {
    totalElement.innerHTML= ((sconto20).toFixed(2) + '€' )
    scontoElement.innerHTML= ('20%')
    resultElement.innerHTML = ('abbiamo applicato uno sconto del 20% sul tuo bilglietto! essendo minorenne il prezzo del tuo biglietto sara  ' + (sconto20).toFixed(2) + '€')
-  }else if (eta > 65) {
+  }else if (eta === 'over') {
     totalElement.innerHTML= ( (sconto40).toFixed(2) + '€' )
    scontoElement.innerHTML= ('40%')
    resultElement.innerHTML = ('abbiamo applicato uno sconto del 40% sul tuo bilglietto! essendo over 65 il prezzo del tuo biglietto sara  ' + (sconto40).toFixed(2) + '€')
@@ -45,6 +43,8 @@ if (!isNaN(km) && !isNaN(eta) && eta > 0 &&  km > 0 && eta < 110) {
 } 
     //ALTRIMENTI nesusn risultato
 else{
+prezzoElement = ('0€')
+kmElement.innerHTML = ('0')
 totalElement.innerHTML= ('0 €')
 scontoElement.innerHTML= ('0%')
 resultElement.innerHTML = ('non è stato possibile eseguire il calcolo')
