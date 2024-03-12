a324ab087f3e4b78dc780a68fd03200bc8afa274
davimuto <davimuto@gmail.com>
2024-03-12 18:55:13 +0100

    add: correzione alcuni errori sconto e prezzo in tabella

--- js/app.js
+++ js/app.js
@@ -16,8 +16,8 @@ const km = parseInt(kmInputElement.value)
     // costanti di prezzi base e sconti (calcolo base km per prezzo base)
 const prezzoBase = 0.21
 const prezzoPerKm = prezzoBase * km
-const sconto20 = prezzoPerKm - (prezzoPerKm * 0.2) 
-const sconto40 = prezzoPerKm - (prezzoPerKm * 0.4) 
+const sconto20 =( prezzoPerKm * 0.2)
+const sconto40 = (prezzoPerKm * 0.4)
 
 
     // stampa elementi in tabella sul dom
@@ -26,15 +26,15 @@ etaElement.innerHTML = (eta)
 prezzoElement.innerHTML =((prezzoPerKm).toFixed(2) + '€') 
 
 // calcola il prezzo effettivo in base all eta 
-if (!isNaN(km)  && km > 0 ) {  
+if (!isNaN(km)  && km > 0 && (!isNaN.prezzoPerKm)) {  
   if (eta === 'minorenne') {
-   totalElement.innerHTML= ((sconto20).toFixed(2) + '€' )
+   totalElement.innerHTML= ((prezzoPerKm - sconto20).toFixed(2) + '€' )
    scontoElement.innerHTML= ('20%')
-   resultElement.innerHTML = ('abbiamo applicato uno sconto del 20% sul tuo bilglietto! essendo minorenne il prezzo del tuo biglietto sara  ' + (sconto20).toFixed(2) + '€')
+   resultElement.innerHTML = ('abbiamo applicato uno sconto del 20% sul tuo bilglietto! essendo minorenne il prezzo del tuo biglietto sara  ' + (prezzoPerKm - sconto20).toFixed(2) + '€')
   }else if (eta === 'over') {
-    totalElement.innerHTML= ( (sconto40).toFixed(2) + '€' )
+    totalElement.innerHTML= ( (prezzoPerKm - sconto40).toFixed(2) + '€' )
    scontoElement.innerHTML= ('40%')
-   resultElement.innerHTML = ('abbiamo applicato uno sconto del 40% sul tuo bilglietto! essendo over 65 il prezzo del tuo biglietto sara  ' + (sconto40).toFixed(2) + '€')
+   resultElement.innerHTML = ('abbiamo applicato uno sconto del 40% sul tuo bilglietto! essendo over 65 il prezzo del tuo biglietto sara  ' + (prezzoPerKm -sconto40).toFixed(2) + '€')
   }else {
     totalElement.innerHTML= ((prezzoPerKm).toFixed(2) + '€' )
    scontoElement.innerHTML= ('0%')
@@ -42,8 +42,8 @@ if (!isNaN(km)  && km > 0 ) {
   }
 } 
     //ALTRIMENTI nesusn risultato
-else{
-prezzoElement = ('0€')
+else {
+prezzoElement.innerHTML = ('0€')
 kmElement.innerHTML = ('0')
 totalElement.innerHTML= ('0 €')
 scontoElement.innerHTML= ('0%')
